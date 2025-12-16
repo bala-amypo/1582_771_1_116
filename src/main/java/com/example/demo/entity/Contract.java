4d0019390a6d5c1be9fb82bd2c7d37d2e67b2f40
bala-amypo <bala@amypo.in>
2025-12-16 10:26:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+public class Contract {
+    private long id;
+    private String contactNumber;
+    private String title;
+    private Date agreedDeliveryDate;
+    private BigDecimal baseContractValue;
+    private String status;
+    private Timestamp createdA
+    
+}
