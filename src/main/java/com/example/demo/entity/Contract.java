67bd2a75ec3e23d234ca3c4db419b5d9525611bb
bala-amypo <bala@amypo.in>
2025-12-16 13:18:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -2,12 +2,22 @@ package com.example.demo.entity;
 
 public class Contract {
     private long id;
-    private String contactNumber;
+    private String contractNumber;
     private String title;
     private Date agreedDeliveryDate;
     private BigDecimal baseContractValue;
     private String status;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+
+
+public Contract(){
+}
+public Contract(long id,String contractNumber,String title,Date agreedDeliveryDate,BigDecimal baseContractValue,String status,Timestamp createdAt,Timestamp updatedAt){
+    this.id=id;
+    this.contractNumber=contractNumber;
+    
+
+}
     
 }
