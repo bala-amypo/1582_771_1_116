7662912467b7f0982c3adcedd33988929adb5c1b
bala-amypo <bala@amypo.in>
2025-12-17 10:26:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/ContractImpl.java
@@ -0,0 +1,53 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Contract;
+
+import com.example.demo.repository.ContractRepository;
+import com.example.demo.service.ContractService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContractImpl implements ContractService {
+
+    private final ContractRepository contractRepository;
+
+    // Constructor Injection
+    public ContractImpl(ContractRepository contractRepository) {
+        this.contractRepository = contractRepository;
+    }
+
+    @Override
+    public Contract createContract(Contract contract) {
+        return contractRepository.save(contract);
+    }
+
+    @Override
+    public Contract updateContract(Long id, Contract contract) {
+        Contract existing = getContractById(id);
+        existing.setAgreedDeliveryDate(contract.getAgreedDeliveryDate());
+        existing.setBaseContractValue(contract.getBaseContractValue());
+        existing.setStatus(contract.getStatus());
+
+        return contractRepository.save(existing);
+    }
+
+   
+
+    @Override
+    public List<Contract> getAllContracts() {
+        return contractRepository.findAll();
+    }
+
+    @Override
+    public void updateContractStatus(Long contractid) {
+        Contract contract = getContractById(contractid);
+
+        // Status updated manually (no delivery logic here)
+        contract.setStatus("UPDATED");
+
+        contractRepository.save(contract);
+    }
+}
+
