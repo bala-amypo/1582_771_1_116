393e2c9cd94d5c9758db52f308432e7e70ba02dc
bala-amypo <bala@amypo.in>
2025-12-17 23:47:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/ContractController.java
+++ src/main/java/com/example/demo/controller/ContractController.java
@@ -2,42 +2,42 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Contract;
 import com.example.demo.service.ContractService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/contracts")
+@RequestMapping("/api/contracts")
 public class ContractController {
 
-    @Autowired
-    private ContractService contractService;
+    private final ContractService contractService;
 
-    @GetMapping
-    public List<Contract> getAllContracts() {
-        return contractService.getAllContracts();
-    }
-
-    @GetMapping("/{id}")
-    public Contract getContractById(@PathVariable Long id) {
-        return contractService.getContractById(id);
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
     }
 
     @PostMapping
-    public Contract createContract(@RequestBody Contract contract) {
+    public Contract create(@RequestBody Contract contract) {
         return contractService.createContract(contract);
     }
 
     @PutMapping("/{id}")
-    public Contract updateContract(@PathVariable Long id,
-                                   @RequestBody Contract contract) {
+    public Contract update(@PathVariable Long id, @RequestBody Contract contract) {
         return contractService.updateContract(id, contract);
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteContract(@PathVariable Long id) {
-        contractService.deleteContract(id);
-        return "Deleted successfully";
+    @GetMapping("/{id}")
+    public Contract getById(@PathVariable Long id) {
+        return contractService.getContractById(id);
+    }
+
+    @GetMapping
+    public List<Contract> getAll() {
+        return contractService.getAllContracts();
+    }
+
+    @PutMapping("/{id}/update-status")
+    public void updateStatus(@PathVariable Long id) {
+        contractService.updateContractStatus(id);
     }
 }
--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -2,65 +2,133 @@ package com.example.demo.entity;
 
 import java.math.BigDecimal;
 import java.security.Timestamp;
+import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+@Entity
+@Table(name = "contracts", uniqueConstraints = @UniqueConstraint(columnNames = "contractNumber"))
 public class Contract {
+    @Id @GeneratedValue
     private long id;
-    private String contractpartyName;
-    private String agreedDeliveryDate;
+    private String contractNumber;
+    private String title;
+    private String counterpartyName;
+    private Date agreedDeliveryDate;
     private BigDecimal baseContractValue;
-    private String Status;
+    private String status;
     private Timestamp createdAt;
     private Timestamp updatedAt;
-    public Contract(String contractpartyName, String agreedDeliveryDate, BigDecimal baseContractValue, String status,
-            Timestamp createdAt, Timestamp updatedAt) {
-        this.contractpartyName = contractpartyName;
+
+    public Contract(){
+
+    }
+    public Contract(String contractNumber, String title, String counterpartyName, Date agreedDeliveryDate,
+            BigDecimal baseContractValue, String status, Timestamp createdAt, Timestamp updatedAt) {
+        this.title = title;
+        this.counterpartyName = counterpartyName;
         this.agreedDeliveryDate = agreedDeliveryDate;
         this.baseContractValue = baseContractValue;
-        Status = status;
+        this.status = status;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
+
+
     public void setId(long id) {
         this.id = id;
     }
-    public void setContractpartyName(String contractpartyName) {
-        this.contractpartyName = contractpartyName;
+
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
     }
-    public void setAgreedDeliveryDate(String agreedDeliveryDate) {
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public void setCounterpartyName(String counterpartyName) {
+        this.counterpartyName = counterpartyName;
+    }
+
+
+    public void setAgreedDeliveryDate(Date agreedDeliveryDate) {
         this.agreedDeliveryDate = agreedDeliveryDate;
     }
+
+
     public void setBaseContractValue(BigDecimal baseContractValue) {
         this.baseContractValue = baseContractValue;
     }
+
+
     public void setStatus(String status) {
-        Status = status;
+        this.status = status;
     }
+
+
     public void setCreatedAt(Timestamp createdAt) {
         this.createdAt = createdAt;
     }
+
+
     public void setUpdatedAt(Timestamp updatedAt) {
         this.updatedAt = updatedAt;
     }
+
+
     public long getId() {
         return id;
     }
-    public String getContractpartyName() {
-        return contractpartyName;
+
+
+    public String getContractNumber() {
+        return contractNumber;
     }
-    public String getAgreedDeliveryDate() {
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getCounterpartyName() {
+        return counterpartyName;
+    }
+
+
+    public Date getAgreedDeliveryDate() {
         return agreedDeliveryDate;
     }
+
+
     public BigDecimal getBaseContractValue() {
         return baseContractValue;
     }
+
+
     public String getStatus() {
-        return Status;
+        return status;
     }
+
+
     public Timestamp getCreatedAt() {
         return createdAt;
     }
+
+
     public Timestamp getUpdatedAt() {
         return updatedAt;
     }
-      
+    
+
+
+    
+    
 }
