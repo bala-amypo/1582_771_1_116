697150f321afb1e3522e7fb0427934e416f580bb
bala-amypo <bala@amypo.in>
2025-12-18 17:04:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/PenaltyCalculationRepository.java
+++ src/main/java/com/example/demo/repository/PenaltyCalculationRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.PenaltyCalculation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-
+@Repository
 public interface PenaltyCalculationRepository extends JpaRepository<PenaltyCalculation, Long> {
     List<PenaltyCalculation> findByContractId(Long contractId);
 }
