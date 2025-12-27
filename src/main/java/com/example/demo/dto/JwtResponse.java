181e1137ef1daf4169d55c9b58fe33ebdd1344cf
bala-amypo <bala@amypo.in>
2025-12-27 15:19:36 +0000

    User commit

--- src/main/java/com/example/demo/dto/JwtResponse.java
+++ src/main/java/com/example/demo/dto/JwtResponse.java
@@ -1,3 +1,54 @@
+// package com.example.demo.dto;
+
+// public class JwtResponse {
+
+//     private String token;
+//     private Long userId;
+//     private String email;
+//     private String role;
+
+//     public JwtResponse() {
+//     }
+
+//     public JwtResponse(String token, Long userId, String email, String role) {
+//         this.token = token;
+//         this.userId = userId;
+//         this.email = email;
+//         this.role = role;
+//     }
+
+//     public String getToken() {
+//         return token;
+//     }
+
+//     public Long getUserId() {
+//         return userId;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public String getRole() {
+//         return role;
+//     }
+
+//     public void setToken(String token) {
+//         this.token = token;
+//     }
+
+//     public void setUserId(Long userId) {
+//         this.userId = userId;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public void setRole(String role) {
+//         this.role = role;
+//     }
+// }
 package com.example.demo.dto;
 
 public class JwtResponse {
@@ -21,28 +72,28 @@ public class JwtResponse {
         return token;
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public Long getUserId() {
+        return userId;
     }
 
     public void setUserId(Long userId) {
         this.userId = userId;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getRole() {
+        return role;
     }
 
     public void setRole(String role) {
