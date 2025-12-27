9e5e7b1ce4c4e664e4995c526005fc33f69c9ab3
bala-amypo <bala@amypo.in>
2025-12-27 15:10:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/BreachRuleRepository.java
+++ src/main/java/com/example/demo/repository/BreachRuleRepository.java
@@ -1,15 +1,26 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.BreachRule;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import java.util.Optional;
+
+// @Repository
+// public interface BreachRuleRepository extends JpaRepository<BreachRule, Long> {
+
+//     Optional<BreachRule> findByRuleName(String ruleName);
+
+//     Optional<BreachRule> findFirstByActiveTrueOrderByIsDefaultRuleDesc();
+// }
 package com.example.demo.repository;
 
 import com.example.demo.entity.BreachRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface BreachRuleRepository extends JpaRepository<BreachRule, Long> {
 
-    Optional<BreachRule> findByRuleName(String ruleName);
-
-    Optional<BreachRule> findFirstByActiveTrueOrderByIsDefaultRuleDesc();
+    Optional<BreachRule> findByIsDefaultRuleTrue();
 }
