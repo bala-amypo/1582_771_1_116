03e6d7c0331b704602801915482799816a0eecdb
bala-amypo <bala@amypo.in>
2025-12-28 07:54:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/BreachRuleController.java
+++ src/main/java/com/example/demo/controller/BreachRuleController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reports")
-public class BreachReportController {
+public class BreachRuleController {
 
     BreachReportService breachReportService;
 
